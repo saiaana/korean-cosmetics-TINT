6715630d8a6b4aa14c0203ff9a9826b12b89c9d7
Saiaana Grigoreva <saygrig@gmail.com>
2026-01-17 23:40:50 +0900

    chages to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import os from "os";
 import ordersRoute from "./routes/orders.js";
 import usersRoute from "./routes/users.js";
 import productsRoute from "./routes/products.js";
@@ -12,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: true, //поменять потом на конкретный URL на проде
+    origin: true, 
     credentials: true,
   }),
 );
@@ -23,19 +22,12 @@ app.use("/api/users", usersRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/cart", cartRoute);
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const HOST = "0.0.0.0";
+
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
-  console.log(`Local: http://localhost:${PORT}`);
-  const networkInterfaces = os.networkInterfaces();
-  Object.keys(networkInterfaces).forEach((interfaceName) => {
-    networkInterfaces[interfaceName].forEach((iface) => {
-      if (iface.family === "IPv4" && !iface.internal) {
-        console.log(`Network: http://${iface.address}:${PORT}`);
-      }
-    });
-  });
 });
+
 
 export default app;
