35466376f4621ca2b8febe4cc1613af5bfd0b65b
Saiaana Grigoreva <saygrig@gmail.com>
2026-01-20 00:26:02 +0900

    Change order in Header menu items

--- src/constants/menuItems.js
+++ src/constants/menuItems.js
@@ -2,9 +2,9 @@ import ROUTES from "./routes";
 
 export const MENU_ITEMS = [
   { label: "BRANDS", path: ROUTES.brands },
+  { label: "CATEGORIES", path: ROUTES.categories },
   { label: "NEW IN", path: ROUTES.newIn },
   { label: "PROMOTIONS", path: ROUTES.promotions },
   { label: "BESTSELLERS", path: ROUTES.bestsellers },
   { label: "BLOG", path: ROUTES.blog },
-  { label: "CATEGORIES", path: ROUTES.categories },
 ];
